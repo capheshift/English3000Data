b4b5e9205750c5b0cd4898b449e6d9fd1cf443e2
Tam Pham <tampham47@live.com>
2015-09-16 00:32:46 +0700

    update data

--- test/index.js
+++ test/index.js
@@ -2,8 +2,12 @@
 var assert = require('assert');
 var wordList = require('../lib');
 
-describe('3000-words-list', function () {
+describe('English3000Data', function () {
   it('should have unit test!', function () {
     assert(true, 'we expected this package author to add actual unit tests.');
+  });
+
+  it('can acess data!', function () {
+    assert((wordList.getLength() === wordList.data.length), 'we expected that word list can return the length of this.');
   });
 });
