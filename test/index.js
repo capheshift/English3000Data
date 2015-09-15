0dc0192d1cbf09277d3a4f5a2a6f99017ecbd051
Tam Pham <tampham47@live.com>
2015-09-15 23:50:18 +0700

    refactor code

--- test/index.js
+++ test/index.js
@@ -1,18 +1,9 @@
-'use strict';
 
 var assert = require('assert');
-var wordsList = require('../lib');
+var wordList = require('../lib');
 
 describe('3000-words-list', function () {
   it('should have unit test!', function () {
     assert(true, 'we expected this package author to add actual unit tests.');
-  });
-
-  it('should have 16 words', function () {
-    assert((wordsList.getLength() === 16), 'we expected this package return 16 fluquent words.');
-  });
-
-  it('should access all of words', function () {
-    assert((wordsList.getAll().length === 16), 'we expected this package return 16 fluquent words.');
   });
 });
